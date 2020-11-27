c8b4cc9c0dfd2c6bccf410b0f5027293134994a0
afeiship <1290657123@qq.com>
2020-11-27 11:38:45 +0800

    Release 1.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,8 @@
  * name: @jswork/next-node-exec
  * description: Node exec for next.
  * homepage: https://github.com/afeiship/next-node-exec
- * version: 1.0.1
- * date: 2020-11-27 11:37:54
+ * version: 1.0.2
+ * date: 2020-11-27 11:38:39
  * license: MIT
  */
 
